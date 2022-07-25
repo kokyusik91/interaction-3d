d44730d862b8fb77f472eba58d77aad2b47c417e
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2022-07-25 09:02:16 +0900

    init files

---
+++ js/wall4d.js
@@ -0,0 +1,7 @@
+(function () {
+  const houseElem = document.querySelector('.house');
+  console.log(houseElem);
+  window.addEventListener('scroll', function () {
+    console.log(pageYOffset);
+  });
+})();
