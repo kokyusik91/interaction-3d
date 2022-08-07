26e7b4e3e3aedd843657e2ecdbc77e8101e193d0
kks91 <kokyusik91@naver.com>
2022-08-07 18:26:07 +0900

    🎨  Add character update

--- js/Character4d.js
+++ js/Character4d.js
@@ -31,6 +31,15 @@ function Character(info){
   this.mainElem.style.left = info.xPos + '%';
   // 스크롤 중인지 아닌지 상태를 저장함.
   this.scrollState = false;
+  // 바로 이전 스크롤 위치 저장
+  this.lastScrollTop = 0;
+  // 각각의 인스턴스의 위치를 조금 더 다루기 쉽게
+  this.xPos = info.xPos;
+  this.speed = info.speed;
+  this.direction;
+  // 좌/우 이동인지 아닌지 판별 하는 상태
+  this.runningState = false;
+  this.rafId;
   // prototype 메소드가 실행됨
   this.init();
 }
@@ -53,6 +62,74 @@ Character.prototype = {
                 self.scrollState = false;
                 self.mainElem.classList.remove('running');
             }, 500);
+
+
+            // 이전 스크롤 위치와 현재 스크롤 위치를 비교
+            if (self.lastScrollTop > scrollY){
+              // 이전 스크롤 값이 현재 스크롤 값보다 크면 : 스크롤을 위로 올린것
+              console.log("밑으로 내림");
+              self.mainElem.setAttribute('data-direction', 'backward');
+            }else {
+              console.log("위로 올림");
+              // 이전 스크롤 값이 현재 스크롤 값보다 작으면 : 스크롤을 아래로 내린것
+              self.mainElem.setAttribute('data-direction', 'forward');
+            }
+
+            self.lastScrollTop = scrollY;
     })
+
+    window.addEventListener('keydown', function(e){
+    if (self.runningState) return;      
+    if(e.key === 'ArrowLeft'){
+        self.mainElem.setAttribute('data-direction', 'left');
+        self.mainElem.classList.add('running');
+        self.direction = 'left';
+        self.run(self);
+        // 누르고 있으면 running중이니깐 true로 바꿔서 keydown 이벤트가 실행되지않게
+        self.runningState = true;
+      }else if(e.key === 'ArrowRight'){
+          self.mainElem.setAttribute('data-direction', 'right');
+          self.mainElem.classList.add('running');
+          self.direction= 'right';
+          self.run(self);
+          self.runningState = true;
+
+      }
+    })
+
+    window.addEventListener('keyup', function(e){
+      self.mainElem.classList.remove('running');
+      cancelAnimationFrame(self.rafId);
+      // 다시 runningState를 false로 바꿔줘서 KeyDown 이벤트가 실행 되도록 만들어준다.
+      self.runningState = false;
+
+    })
+  },
+
+  run : function(self){
+    if(self.direction === 'left'){
+         // 이동할때 현재 xPos에서 이동하는 스피드 만큼 빼준다.
+        self.xPos -= self.speed;
+
+    }else if(self.direction === 'right'){
+              // 이동할때 현재 xPos에서 이동하는 스피드 만큼 빼준다.
+        self.xPos += self.speed;
+        // 캐릭터 인스턴스 left위치를 위에서 빼준 값만큼 최신화 된값을 left값으로 최신화 해준다.
+    }
+
+  // xPos이 넘어가는 순간에 안넘어 가게 만들어준다.
+  if (self.xPos < 2) {
+      self.xPos = 2;
+  }
+
+   if (self.xPos > 88) {
+      self.xPos = 88;
+  }
+
+    self.mainElem.style.left = self.xPos + '%';
+    // requestAnimationFrame이 reutrn 하는 숫자값을 나중에 cancelAnimationFrame에도 쓴다.
+    self.rafId = requestAnimationFrame(function () {
+            self.run(self);
+        });
   }
 }
